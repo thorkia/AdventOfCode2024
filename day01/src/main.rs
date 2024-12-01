890e39b7de85628ba34f6c20348874df747e8f02
thorkia <thorkia@gmail.com>
2024-12-01 11:42:30 -0500

    Day 1 solution and inputs.  First stab at a working rust solution

---
+++ day01/src/main.rs
@@ -0,0 +1,86 @@
+
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn lines_from_file<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn part1(lines: &[String]) {
+    let mut col1: Vec<i32> = Vec::new();
+    let mut col2: Vec<i32> = Vec::new();
+
+    for line in lines {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let part1 = parts[0].parse::<i32>().unwrap();
+        let part2 = parts[1].parse::<i32>().unwrap();
+        
+        col1.push(part1);
+        col2.push(part2);
+    
+    }
+
+    //Sort the rows
+    col1.sort();
+    col2.sort();
+
+    //Iterate over the rows
+    let mut row_diff = 0;
+    for i in 0..col1.len() {
+        row_diff += (col1[i] - col2[i]).abs();
+    }
+
+    println!("{}", row_diff);
+}
+
+fn part2(lines: &[String]) {
+    let mut col1: Vec<i32> = Vec::new();
+    let mut col2: HashMap<i32, i32> = HashMap::new();
+
+    for line in lines {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let part1 = parts[0].parse::<i32>().unwrap();
+        let part2 = parts[1].parse::<i32>().unwrap();
+        
+        col1.push(part1);
+        if col2.contains_key(&part2) {
+            col2.insert(part2, col2.get(&part2).unwrap() + 1);
+        } else {
+            col2.insert(part2, 1);
+        }
+    }
+
+    //Iterate over the rows
+    let mut total: i64 = 0;
+    for i in 0..col1.len() {
+        if col2.contains_key(&col1[i]) {
+            let count = col2.get(&col1[i]).unwrap();
+            total += i64::from( (col1[i] * count).abs() );
+        }
+    }
+
+    println!("{}", total);
+
+}
+
+fn main() {
+    let Ok(lines) = lines_from_file("./src/input.txt") else {
+        panic!("Could not read file");
+    };
+
+    let mut processed_lines: Vec<String> = Vec::new();
+    for line in lines {
+        if let Ok(ip) = line {
+            processed_lines.push(ip);
+        }
+    }
+
+    part1(&processed_lines);
+    part2(&processed_lines);
+}
---
+++ day01/src/readme.md
@@ -0,0 +1,14 @@
+Day 1 Part 1
+
+We have 2 lists of numbers. We need to find difference betweem the lowest in each, then the next lowest, and so on.
+We sum those differences, and that is our answer.
+
+We will extract both lists, sort them, then add the difference of each element to a running total.
+
+Day 1 Part 2
+
+When we parse list 1, we will turn it into a list
+
+We will do the same with list 2, but incrementing the count for each value.
+
+Then we will iterate through list 1 and multiply it by the value in the hasmap, and keep a running total.
